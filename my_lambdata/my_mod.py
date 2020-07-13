4920402865912cea619d995a0a092ff8f7dc7806
DevvinK <devvink@gmail.com>
2020-07-12 20:23:54 -0600

    Added two functions for class assignment, null checking and date dividing

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,8 +1,25 @@
 # my_lambdata/my_mod.py
 # my_lambdata.my_mod
 
+import pandas as pd
+
 def enlarge(num):
    return num * 100
+
+def null_check(df):
+   null_lines = df[df.isnull().any(axis=1)]
+   return null_lines
+
+def date_divider(df,date_col):
+   '''
+    df: the whole dataframe adding new day, month, year to
+    date_col: the name of the column the date is stored in
+   '''
+   converted_df = df.copy()
+   converted_df["Year"] = pd.DatetimeIndex(converted_df[date_col]).year
+   converted_df["Month"] = pd.DatetimeIndex(converted_df[date_col]).month
+   converted_df["Day"] = pd.DatetimeIndex(converted_df[date_col]).day
+   return converted_df
 
 if __name__ == "__main__":
    x = 11
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,13 +1,26 @@
 # my_lambdata/my_script.py
-# my_lambdata.my_script
+# python -m my_lambdata.my_script
 
-from pandas import DataFrame
-from my_lambdata.my_mod import enlarge
+import pandas as pd
+from my_lambdata.my_mod import enlarge, null_check, date_divider
+import numpy as na
+
 
 print("Hello")
 
-df = DataFrame({"a":[1,2,3], "b":[4,5,6]})
+df = pd.DataFrame({"a":[1,2,3], "b":[4,5,6]})
 print(df.head())
+
+df2 = pd. DataFrame({"a":[na.nan,2,3], "b":[4,5,6]})
+print(null_check(df2))
+
+raw_data = {'name': ['Willard Morris', 'Al Jennings', 'Omar Mullins', 'Spencer McDaniel'],
+'age': [20, 19, 22, 21],
+'favorite_color': ['blue', 'red', 'yellow', "green"],
+'grade': [88, 92, 95, 70],
+'birth_date': ['01-02-1996', '08-05-1997', '04-28-1996', '12-16-1995']}
+df3 = pd.DataFrame(raw_data, index = ['Willard Morris', 'Al Jennings', 'Omar Mullins', 'Spencer McDaniel'])
+print(date_divider(df3,'birth_date'))
 
 x=11
 print(enlarge(x))
